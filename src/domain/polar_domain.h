9a06d692d24c042cfe15f74047ed17a84967e919
GyeonghunKim <aass98998@snu.ac.kr>
2022-06-04 01:58:07 +0900

    Add PolarDomain

--- src/domain/polar_domain.h
+++ src/domain/polar_domain.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "base_domain.h"
+class PolarSpatialGrid : public BaseSpatialGrid
+{
+public:
+    PolarSpatialGrid();
+    PolarSpatialGrid(int num_grid_1, int num_grid_2, double r_start_, double r_end_);
+
+private:
+    double r_start, r_end;
+};
+
+class PolarDomain : public BaseDomain
+{
+public:
+    PolarDomain();
+    PolarDomain(int num_grid_1, int num_grid_2, double t_start, double t_end, int num_times, double r_start, double r_end);
+};
