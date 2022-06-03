f78c9aae7030d99f48615406f7d6f680e9bfffac
GyeonghunKim <aass98998@snu.ac.kr>
2022-06-04 02:03:03 +0900

    Added test_all_polar_domain in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "test/test_domain/test_base_domain.cpp"
+#include "test/test_domain/test_polar_domain.cpp"
 #include "test/test_domain/test_rectangular_domain.cpp"
 
 int main(int argc, char **argv)
 {
     test_all_base_domain();
     test_all_rectangular_domain();
+    test_all_polar_domain();
 }
