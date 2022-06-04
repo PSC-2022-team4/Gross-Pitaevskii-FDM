841df2e0358f65b3afc67074adf764cf4affe345
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-06-04 15:07:30 +0900

    domain in solver changed into pointer type

--- src/initial_condition/initial_condition.h
+++ src/initial_condition/initial_condition.h
@@ -7,6 +7,7 @@ class InitialCondition
     private:
         std::function<std::complex<double>(double, double)> initial_condition_function;
     public:
+        InitialCondition() = default;
         InitialCondition(std::function<std::complex<double>(double, double)> initial_condition_function);
         void assign_to_domain(BaseDomain *domain);
 };
--- src/serial_solver/base_serial_solver.cpp
+++ src/serial_solver/base_serial_solver.cpp
@@ -7,5 +7,14 @@
  * @param potential_func_ 
  * @param g_ 
  */
-BaseSolver::BaseSolver(InitialCondition initialCondition_, std::function<double(double, double)> potential_func_, double g_)
-    : initialCondition(initialCondition_), potential_func(potential_func_), g(g_){};
+BaseSolver::BaseSolver(InitialCondition initialCondition_, std::function<double(double, double)> potential_func_, double g_, BaseDomain baseDomain_)
+    : initialCondition(initialCondition_), potential_func(potential_func_), g(g_), domain(&baseDomain_){};
+
+/**
+ * @brief defualt temporal equation 
+ * 
+ * @return std::complex<double> 0+0i
+ */
+std::complex<double> BaseSolver::temporal_equation(int i, int j, int k){
+    return std::complex<double> {0};
+}
