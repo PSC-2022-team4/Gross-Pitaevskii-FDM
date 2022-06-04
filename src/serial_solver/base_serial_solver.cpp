ee3ad38bf9c8eda67b513cfaa394aee8e0f3dd44
GyeonghunKim <aass98998@snu.ac.kr>
2022-06-04 14:24:46 +0900

    Update base solver

--- src/serial_solver/base_serial_solver.cpp
+++ src/serial_solver/base_serial_solver.cpp
@@ -2,13 +2,10 @@
 
 /**
  * @brief Construct a new Base Solver:: Base Solver object
- *        Default constructor
  * 
+ * @param initialCondition_ 
+ * @param potential_func_ 
+ * @param g_ 
  */
-BaseSolver::BaseSolver(){};
-
-BaseSolver::BaseSolver(InitialCondition initialCondition){
-    
-    this -> initialCondition = initialCondition; 
-
-};
+BaseSolver::BaseSolver(InitialCondition initialCondition_, std::function<double(double, double)> potential_func_, double g_)
+    : initialCondition(initialCondition_), potential_func(potential_func_), g(g_){};
--- src/serial_solver/forward_euler/forward_euler_rectangular_solver.h
+++ src/serial_solver/forward_euler/forward_euler_rectangular_solver.h
@@ -4,11 +4,12 @@
 #include <functional>
 #include "src/domain/base_domain.h"
 #include "src/initial_condition/initial_condition.h"
-class BaseSolver
+#include "src/serial_solver/base_serial_solver.h"
+class ForwardEulerRectangularSolver : BaseSolver
 {
 public:
-    BaseSolver() = default;
-    BaseSolver(InitialCondition initialCondition, std::function<double(double, double)> potential, double g);
+    ForwardEulerRectangularSolver() = default;
+    ForwardEulerRectangularSolver(InitialCondition initialCondition, std::function<double(double, double)> potential, double g);
 
 protected:
     InitialCondition initialCondition;
@@ -16,4 +17,5 @@ protected:
     std::function<double(double, double)> potential_func;
     double g;
     std::complex<double> temporal_equation(double x, double y, double t);
+    void update();
 };
