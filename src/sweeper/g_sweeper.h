63f093d5b5ddb49dcf2a8a8e13d5586d1db5f2f9
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-06-17 00:05:43 +0900

    comments in header file

--- src/sweeper/g_sweeper.h
+++ src/sweeper/g_sweeper.h
@@ -1,7 +1,21 @@
+/**
+ * @file g_sweeper.h
+ * @author Minyoung Kim, Gyeonghun Kim
+ * @brief Header for g parameter sweeper class 
+ * @version 0.1
+ * @date 2022-06-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include "./base_sweeper.h"
 #include "../potential/harmonic_potential.h"
 #include "../initial_condition/initial_condition.h"
-
+/**
+ * @brief g parameter sweeper class that inherits Base sweeper class.
+ * 
+ */
 class GSweeper:public BaseSweeper{
     public:
         GSweeper() = default;
